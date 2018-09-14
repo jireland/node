09455d09265e2ad331bc1b9f07681164d3a8fd86
Jonathon Ireland <jonathon.ireland@dcsg.com>
2018-09-14 15:00:54 -0400

    added html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('HEllo World!');
+  res.send('<ul><li>Successful push</li><li>Successful push with html</li></ul>');
 });
 
 
